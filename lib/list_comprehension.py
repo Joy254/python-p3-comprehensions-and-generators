eff15c01e1413e29965670c10364b1db635c097e
JOY254 <nyorgijoy@gmail.com>
2023-11-24 14:18:32 -0600

    working through

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -2,6 +2,12 @@
 
 def return_evens(num_list):
     pass
+ return [num for num in num_list if num % 2 == 0]
+
 
 def make_exclamation(sentence_list):
     pass
+    if not sentence_list:
+        return ["I like computers", "I require coffee","Live long and prosper",]
+        return ["I like computers!","I require coffee!","Live long and prosper!",
+for sentence in sentence_list]
